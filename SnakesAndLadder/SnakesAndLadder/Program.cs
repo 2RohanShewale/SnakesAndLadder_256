8cdc16dd64c4c1527779368259cb20c0b0f16fb9
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-04 19:54:46 +0530

    [Rohan]UC2: The player rolles the die

--- SnakesAndLadder/SnakesAndLadder/Program.cs
+++ SnakesAndLadder/SnakesAndLadder/Program.cs
@@ -10,8 +10,16 @@ namespace SnakesAndLadder
             int startPosition = 0;
 
             Player player_One = new Player(startPosition);
-
-            Console.WriteLine($"You are at position : {player_One.position}");
+            Random rand = new Random();
+            while (true)
+            {
+                Console.WriteLine($"You are at position : {player_One.position}");
+                Console.WriteLine("Press any key to roll"); Console.ReadKey();
+                int roll = rand.Next(0, 7);
+                Console.WriteLine($"You have Rolled {roll}");
+                player_One.position += roll;
+                
+            }
 
             Console.ReadKey();
         }
