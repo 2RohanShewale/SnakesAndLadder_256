7aa69548d650cbde031c1bb8ffc6e5e51eee136d
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-04 22:48:07 +0530

    [Rohan]UC4: Repeating until player gets 100

--- SnakesAndLadder/SnakesAndLadder/Program.cs
+++ SnakesAndLadder/SnakesAndLadder/Program.cs
@@ -11,14 +11,21 @@ namespace SnakesAndLadder
 
             Player player_One = new Player(startPosition);
             Random rand = new Random();
-            while (true)
+            while (player_One.position <= 100)
             {
                 Console.WriteLine($"\nYou are at position : {player_One.position}");
                 Console.WriteLine("Press any key to roll"); Console.ReadKey();
                 int roll = rand.Next(1, 7);
                 Console.WriteLine($"\nYou have Rolled {roll}");
+                
                 Game game = new Game();
                 player_One.position = game.AfterRoll(roll, player_One.position);
+                if (player_One.position < 0)
+                {
+                    Console.WriteLine("\n**You went under zero starting Again**");
+                    player_One.position = 0;
+                }
+
             }
 
             Console.ReadKey();
