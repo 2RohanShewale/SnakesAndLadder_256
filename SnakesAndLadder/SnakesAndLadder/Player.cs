3d310bbd21a53b960e52d62af8eb1493c8430e8c
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-04 19:45:16 +0530

    [Rohan]UC1: With single player at startPosition

---
+++ SnakesAndLadder/SnakesAndLadder/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLadder
+{
+    public class Player
+    {
+        public int position;
+
+        public Player(int position)
+        {
+            this.position = position;
+        }
+    }
+}
---
+++ SnakesAndLadder/SnakesAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakesAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*_________Sankes And Ladder_________*");
+            int startPosition = 0;
+
+            Player player_One = new Player(startPosition);
+
+            Console.WriteLine($"You are at position : {player_One.position}");
+
+            Console.ReadKey();
+        }
+    }
+}
